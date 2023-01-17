899fa4fbf3cfeff47b86e81d8a94111584939e29
Shadowskin5 <107371835+Shadowskin5@users.noreply.github.com>
2023-01-17 09:03:28 -0500

    kiokiki

--- Scripts/ChildScript.cs
+++ Scripts/ChildScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChildScript : MonoBehaviour
 {
-    private bool isRight = true;
+    private bool isRight = true; //r
     private GameObject parent;
     private Rigidbody2D rb;
     public GameObject curPointHit;
